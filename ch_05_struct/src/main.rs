71ec8322c6a5b52b63502cd7fa4553935525fe3c
Lashi <lashi0812@gmail.com>
2024-03-15 05:03:00 +0000

    struct

--- ch_05_struct/src/main.rs
+++ ch_05_struct/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::fmt::Debug;
 
 fn using_struct_update_syntax() {
@@ -23,6 +24,94 @@ fn using_struct_update_syntax() {
     println!("{:#?}\n{:#?}", rust, python);
 }
 
+fn struct_example() {
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    impl Rectangle {
+        fn area(self: &Self) -> u32 {
+            self.width * self.height
+        }
+    }
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 40,
+    };
+
+    println!("{:#?} has area : {}", rect1, rect1.area());
+}
+
+fn struct_can_have_multiple_impl_block() {
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    impl Rectangle {
+        fn area(self: &Self) -> u32 {
+            self.width * self.height
+        }
+    }
+
+    impl Rectangle {
+        fn can_hold(&self, other: &Self) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+    }
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 40,
+    };
+
+    let rect2 = Rectangle {
+        width: 20,
+        height: 10,
+    };
+    let rect3 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("can rect1 hold rect2? : {} ", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? : {} ", rect1.can_hold(&rect3));
+}
+
+fn associative_function() {
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    impl Rectangle {
+        fn square(size: u32) -> Self {
+            Rectangle {
+                width: size,
+                height: size,
+            }
+        }
+    }
+
+    impl Rectangle {
+        fn area(&self) -> u32 {
+            self.width * self.height
+        }
+    }
+
+    let square = Rectangle::square(30);
+    println!("{:#?} has area : {}", square, square.area());
+}
+
+
 fn main() {
     using_struct_update_syntax();
+    struct_example();
+    struct_can_have_multiple_impl_block();
+    associative_function();
 }
